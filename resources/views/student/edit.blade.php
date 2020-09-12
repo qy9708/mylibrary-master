9d24cadd3bdd2d0468ddd84d675bca751970f212
qy9708 <61824766+qy9708@users.noreply.github.com>
2020-09-12 20:59:33 +0800

    create view for log

---
+++ resources/views/student/edit.blade.php
@@ -0,0 +1,54 @@
+<?php
+ use App\Common;
+ ?>
+ @extends('layout.app')
+
+ @section('content')
+
+	 <!-- Bootstrap Boilerplate... -->
+
+	 <div class="panel-body">
+	 <button><a href="{{ url('/logindex') }}">Return to Log Records</a></button>
+			 {!! Form::model($log, [
+			 'route' => ['student.update', $log->id],
+			 'method' => 'put',
+			 'class' => 'form-horizontal',
+			 ]) !!}
+
+			<div class="form-group_row">
+				{!! Form::label('log-week','Week',['class' => 'control-label col-sm-3',]) !!}
+				<div class="col-sm-9">
+					{!! Form::text('week',null,[
+						'id'		=> 'log-week',
+						'class'		=> 'form-control',
+						'maxlength' => 10,
+					]) !!}
+				</div>
+			<p>{{ $errors->getBag('default')->first('week') }}</p>
+			</div>
+		<br></br>
+			<div class="form-group row">
+				{!! Form::label('firm-description','Description',['class' => 'control-label col-sm-3',]) !!}
+				<div class="col-sm-3">
+          {!! Form::text('description',null,[
+            'id'		=> 'log-description',
+            'class'		=> 'form-control',
+            'maxlength' => 5000,
+          ]) !!}
+				</div>
+        <p>{{ $errors->getBag('default')->first('description') }}</p>
+			</div>
+      <br></br>
+			 <!-- Submit Button -->
+			<div class="form-group_row">
+				<div class="col-sm-offset-3 col-sm-6">
+					{!! Form::button('Update',[
+						'type'		=> 'submit',
+						'class'		=> 'btn_btn-primary',
+					]) !!}
+				</div>
+			</div>
+			 {!! Form::close() !!}
+		 </div>
+
+ @endsection
