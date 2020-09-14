1866cfe1f5a4a63156f7365cd2aded77f9a0bed1
qy9708 <61824766+qy9708@users.noreply.github.com>
2020-09-14 22:50:52 +0800

    document controller store with validation

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -58,11 +58,20 @@ class DocumentController extends Controller
      */
     public function store(Request $request)
     {
-      $documents = new Document();
-      $documents->fill($request->all());
-      $documents->save();
-
-      return redirect()->route('document.index');
+      request()->validate([
+            'file' => 'required',
+            'file.*' => 'mimes:doc,pdf,docx,txt,zip,jpeg,jpg,png'
+        ]);
+        if($request->hasfile('file')) {
+            foreach($request->file('file') as $file)
+            {
+                $fileName = time().rand(0, 1000).pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $fileName = $fileName.'.'.$file->getClientOriginalExtension();
+                $file->move(public_path(),$fileName);
+                $input['file'] = $filename;
+                Document::create($input);
+            }
+        return redirect()->route('document'); 
     }
 
     /**
